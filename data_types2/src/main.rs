e26b8865517e0a51cf8590a46303f02579f408fb
code0-god <code0.sin@gmail.com>
2025-11-25 01:14:00 +0900

    feat: study upto 3.2. Data Types

---
+++ data_types2/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("tuple: x.0={five_hundred}, x.1={six_point_four}, x.2={one}");
+}
